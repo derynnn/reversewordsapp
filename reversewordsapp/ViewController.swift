31610297e8421ee4046f305bacae1c5119f0c13d
derynnn <ana.tochilova@gmail.com>
2024-05-03 13:46:39 +0300

    Added to the base code, created storyboard

--- reversewordsapp/ViewController.swift
+++ reversewordsapp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func reverseButtonTapped(_ sender: UIButton) {
+        guard let inputText = inputTextField.text else {
+            return
+        }
+        let reversedWords = reverseWords(in: inputText)
+        resultLabel.text = reversedWords
+    }
+    
+    private func reverseWords(in input: String) -> String {
+        let words = input.components(separatedBy: .whitespaces)
+        let reversedWords = words.map { String($0.reversed()) }
+        return reversedWords.joined(separator: " ")
+    }
 }
-
