d517b8cbdafaa7d6d474a78326be1b0a5632cfc3
derynnn <ana.tochilova@gmail.com>
2024-05-13 11:41:04 +0300

    Added pragma marks, changed class to final

--- reversewordsapp/Views/ReverseViewController.swift
+++ reversewordsapp/Views/ReverseViewController.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 
-class ReverseViewController: UIViewController {
+final class ReverseViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: - IBActions
+    
     @IBAction func reverseButtonTapped(_ sender: UIButton) {
         guard let inputText = inputTextField.text else {
             return
@@ -23,6 +29,8 @@ class ReverseViewController: UIViewController {
         let reversedWords = reverseWords(in: inputText)
         resultLabel.text = reversedWords
     }
+    
+    // MARK: - Private Methods
     
     private func reverseWords(in input: String) -> String {
         let words = input.components(separatedBy: .whitespaces)
